51b7e93fc7e03318d88800b0d3e4ced4ae9dec7b
sadharuuu <165069038+sadharuuu@users.noreply.github.com>
2026-06-12 23:39:23 +0530

    update done2

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/uploadMiddleware")
-const { updateProfile } = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
 
 const {
   registerUser,
